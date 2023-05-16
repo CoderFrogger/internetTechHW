b0e280c5518f69797b5d866b634ae9d6c0d157b6
CoderFrogg <ignas.paulauskis@stud.vilniustech.lt>
2023-05-16 23:55:12 +0300

    Login, Signup & Dashboard

--- ITHomeWork/src/views/signup.jsx
+++ ITHomeWork/src/views/signup.jsx
@@ -1,7 +1,27 @@
+import {Link} from "react-router-dom";
+
 export default function Signup() {
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+    }
+
     return (
-        <div>
-            <h1>Signup</h1>
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Create an account</h1>
+                    <input type="email" placeholder="Email"/>
+                    <input type="fName" placeholder="First Name"/>
+                    <input type="lName" placeholder="Last Name"/>
+                    <input type="username" placeholder="Username"/>
+                    <input type="password" placeholder="Password"/>
+                    <button className="btn btn-block">Login</button>
+                    <p className="message">
+                        Already have an account? <Link to="/login">login here</Link>
+                    </p>
+                </form>
+            </div>
         </div>
     )
 }
