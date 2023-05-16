4bcc63d22a328fb9a84f4251f8c5f46b1d7b67b9
CoderFrogg <ignas.paulauskis@stud.vilniustech.lt>
2023-05-16 23:36:30 +0300

    Auth

--- ITHomeWork/src/router.jsx
+++ ITHomeWork/src/router.jsx
@@ -1,21 +1,44 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import NotFound from "./views/notFound.jsx";
 import Signup from "./views/signup.jsx";
 import Login from "./views/login.jsx";
 import Users from "./views/users.jsx";
+import DefaultLayout from "./components/layouts/defaultLayout.jsx";
+import GuestLayout from "./components/layouts/guestLayout.jsx";
+import Dashboard from "./views/dashboard.jsx";
 
 const router = createBrowserRouter([
     {
-        path: "/login",
-        element: <Login/>
+        path: "/",
+        element: <DefaultLayout/>,
+        children: [
+            {
+                path: "/",
+                element: <Navigate to="/users"/>
+            },
+            {
+                path: "/users",
+                element: <Users/>
+            },
+            {
+                path: "/dashboard",
+                element: <Dashboard/>
+            },
+        ]
     },
     {
-        path: "signup",
-        element: <Signup/>
-    },
-    {
-        path: "/users",
-        element: <Users/>
+        path: "/",
+        element: <GuestLayout/>,
+        children: [
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "signup",
+                element: <Signup/>
+            },
+        ]
     },
     {
         path: "*",
